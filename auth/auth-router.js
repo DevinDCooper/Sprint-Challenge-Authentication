054905ecc4b3fee24a09da3a63f6f19c486f23d1
Devin Cooper <37602228+DevinDCooper@users.noreply.github.com>
2020-06-12 19:24:34 -0700

    added test db3

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,79 @@
+const bcryptjs = require("bcryptjs");
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const secrets = require('../passcode/secret.js');
+
+const Users = require("../users/users-model.js")
+const { isValid } = require("../users/users-service.js")
 
 router.post('/register', (req, res) => {
   // implement registration
+  const cred = req.body;
+  if (isValid(cred)) {
+    const rounds = process.env.BCRYPT_ROUNDS || 8;
+
+    const hash = bcryptjs.hashSync(cred.password, rounds);
+
+    cred.password = hash;
+
+
+    Users.add(cred)
+    .then(user => {
+      res.status(201).json({data: user});
+    })
+    .catch(error => {
+      res.status(500).json({ message: error.message });
+
+    })
+  } else {
+    res.status(400).json({ message:"plz provide username and password",
+   });
+  }
 });
 
 router.post('/login', (req, res) => {
   // implement login
+const { username, password } = req.body;
+
+if (isValid(req.body)) {
+ Users.findBy({ username : username })
+ .then(([user]) => {
+
+
+  if (user && bcryptjs.compareSync(password, user.password)) {
+    const token = generateToken(user);
+    res.status(200).json({
+      message: "I knew you would make it!",
+      token
+    })
+  } else {
+    res.status(401).json({ message: 'who are you? check your creds'})
+  }
+ })
+ .catch(error => {
+   res.status(500).json({ message: error.message});
+ });
+
+} else {
+  res.status(400).json({
+    message:  "please provide username and password and the password should be alphanumeric"
+  });
+}
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: '2h'
+  };
+  return jwt.sign(payload, secrets.jwtSecret,options)
+
+}
+
+
+
 
 module.exports = router;
